cceeda065404aa939cdcb40a2e9d20018cba4ffe
dbehdrnr0202 <bb06001@naver.com>
2023-06-22 12:13:26 +0900

    param added to shell script

--- code/test.sh
+++ code/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 param_instance=""
 param_memory=""
+param_spark=""
+param_hdfs=""
 empty_str=""
 while [ -n "$1" ]
 do
@@ -8,6 +10,10 @@ do
         -i) $param_instance=$2
             shift;;
         -m) $param_memory=$2
+            shift;;
+        -s) $param_spark=$2
+            shift;;
+        -h) $param_hdfs=$2
             shift;;
         *) ;;
     esac
@@ -22,6 +28,14 @@ if [ ${#param_memory} == 0 ]
 then
     param_m='4G'
 fi
+if [ ${#param_spark} == 0 ]
+then
+    param_s='spark://mater:7077'
+fi
+if [ ${#param_hdfs} == 0 ]
+then
+    param_h='hdfs://master:9000'
+fi
 
 hdfs dfs -put ./test_img/* /test
-python test_load_model.py -i $param_i -m $param_m
+python test_load_model.py -i $param_i -m $param_m -s $param_s -h $param_h
